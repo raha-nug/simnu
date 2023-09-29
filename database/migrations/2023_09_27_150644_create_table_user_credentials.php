2832f028c969abd7d95a3a23d13b99135ec66f70
raha-nug <rival10221029@student.sttcipasung.ac.id>
2023-09-29 16:28:57 +0700

    error table

--- database/migrations/2023_09_27_150644_create_table_user_credentials.php
+++ database/migrations/2023_09_27_150644_create_table_user_credentials.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->boolean('can_manage_user');
             $table->timestamps();
 
-            $table->foreign('id_grup')->references('id')->on('user_groups')->onDelete('cascade');
+            $table->foreign('id_grup')->references('id')->on('table_user_groups')->onDelete('cascade');
         });
     }
 
