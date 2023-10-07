26ea8f61a9507d1932a7a0b9b62af4a73c89290f
Ciman Code <firmanabdul278@gmail.com>
2023-10-07 10:59:34 +0700

    (+) use usergroup

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Users;
+use App\Models\UserGroup;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -28,17 +29,19 @@ class LoginController extends Controller
             Alert::error('Oops!','Password Salah');
             return redirect()->back();
         }
-        $credentials = UserGroup::select('table_user_group.id', 'table_user_groups.nama_grup', 'table_credentials.id',
-                                         'table_credentials.id_grup', 'table_credentials.can_create', 'table_credentials.can_update',
-                                         'table_credentials.can_delete', 'table_credentials.can_manage_user')
-                                ->leftjoin('table_credentials', 'table_user_groups.id', '=', 'table_credentials.id_grup')
-                                ->where('table_user_groups.id', $users->id_grup)
-                                ->first();
+        $credentials = UserGroup::query()->where('id', $users->id_grup)->first();
+        // $credentials = UserGroup::select('table_user_groups.id', 'table_user_groups.nama_grup', 'table_user_credentials.id',
+        //                                  'table_user_credentials.id_grup', 'table_user_credentials.can_create', 'table_user_credentials.can_update',
+        //                                  'table_user_credentials.can_delete', 'table_user_credentials.can_manage_user')
+        //                         ->join('table_user_credentials', 'table_user_groups.id', '=', 'table_user_credentials.id_grup')
+        //                         ->where('table_user_groups.id', $users->id_grup)
+        //                         ->first();
+        // dd($credentials);
         if(!session()->isStarted())
             session()->start();
             session()->put('logged','yes',true);
             session()->put('id_users',$users->id);
-            session()->put('id_credentials', $credentials->id);
+            // session()->put('id_credentials', $credentials->id);
         Alert::success('Login Berhasil');
         return redirect()->route('dashboard');
     }
