57f3d91cf0d11e3b49452a029fff32ab57eae9e0
Ciman Code <firmanabdul278@gmail.com>
2023-10-07 13:25:20 +0700

    (+) akses fix

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,19 +32,18 @@ class LoginController extends Controller
         $credentials = UserGroup::join('table_user_credentials', 'table_user_groups.id', '=', 'table_user_credentials.id_grup')
                                 ->where('table_user_groups.id', $users->id_grup)
                                 ->first();
-        // dd($credentials);
         $HakAkses = $this->checkAkses($credentials);
         if(!session()->isStarted())
             session()->start();
             session()->put('logged','yes',true);
             session()->put('id_users',$users->id);
-            session()->put('hak_akses', $HakAkses['hak_akses']);
+            session()->put('hak_akses', $HakAkses['hak_akses'] ?? "");
             session()->put('can_create', $credentials->can_create);
             session()->put('can_update', $credentials->can_update);
             session()->put('can_delete', $credentials->can_delete);
             session()->put('can_manage_user', $credentials->can_manage_user);
-        Alert::success('Login Berhasil');
-        return redirect()->route('dashboard');
+            Alert::success('Login Berhasil');
+            return redirect($HakAkses['url']);
     }
 
     public function logout(){
@@ -52,15 +51,15 @@ class LoginController extends Controller
         return redirect();
     }
 
-    private function checkAkses($credentials) {
+    protected function checkAkses($credentials) {
         if($credentials->id_pwnu){
             return ['hak_akses' => $credentials->id_pwnu, 'url' => route('dashboard')];
         } elseif($credentials->id_pcnu) {
-            return ['hak_akses' => $credentials->id_pcnu, 'url' => route('pcnu-detail') . 'page=10&pc=' . setRoute($credentials->id_pcnu)];
+            return ['hak_akses' => $credentials->id_pcnu, 'url' => route('pcnu-detail') . '?page=10&pc=' . setRoute($credentials->id_pcnu)];
         } elseif ($credentials->id_mwcnu) {
-            return ['hak_akses' => $credentials->id_mwcnu, 'url' => route('mwc-list-bypcnu') . 'page=10&mwc=' . setRoute($credentials->id_mwcnu)];
+            return ['hak_akses' => $credentials->id_mwcnu, 'url' => route('mwc-list-bypcnu') . '?page=10&mwc=' . setRoute($credentials->id_mwcnu)];
         } elseif ($credentials->id_rantingnu) {
-            return ['hak_akses' => $credentials->id_rantingnu, 'url' => route('ranting-list-bymwc'). 'page=10&ranting' . setRoute($credentials->id_rantingnu)];
+            return ['hak_akses' => $credentials->id_rantingnu, 'url' => route('ranting-list-bymwc'). '?page=10&ranting' . setRoute($credentials->id_rantingnu)];
         } else {
             return redirect(route('dashboard'));
         }
