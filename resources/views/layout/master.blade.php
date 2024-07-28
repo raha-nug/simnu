dd647cb23e20ab372d2d2263ea9d37e654ec3f86
Ciman Code <firmanabdul278@gmail.com>
2024-07-28 13:52:45 +0700

    (+) hero

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -45,7 +45,7 @@
       <div class="d-flex align-items-center justify-content-between">
         <a href="" class="logo d-flex align-items-center">
           <img src="{{ url('/') }}/assets/img/logo.svg" class="ms-lg-4" alt="" />
-          <span class="d-none d-lg-block">SIM NU</span>
+          <span class="d-none d-lg-block">SIM NU JABAR</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
       </div>
